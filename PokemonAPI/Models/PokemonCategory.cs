614e78dd7e247ca170b0a3935c2bae5cd534bed7
Rishav Das <iamrishavdas@gmail.com>
2025-02-24 10:26:02 +0530

    Updated the Models and Added the DataContext

--- PokemonAPI/Models/PokemonCategory.cs
+++ PokemonAPI/Models/PokemonCategory.cs
@@ -4,7 +4,6 @@
     {
         public int PokemonId { get; set; }
         public int CategoryId { get; set; }
-
         public Pokemon Pokemon { get; set; }
         public Category Category { get; set; }
     }
